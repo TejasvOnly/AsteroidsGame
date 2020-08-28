6e108f5d20b365eb8bebb0517ba0d3c7e9f83fb3
Tejasv Sharma <9414297797.ts@gmail.com>
2020-08-28 13:12:10 +0530

    added enemies

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -11,7 +11,7 @@ const graphicAssets = {
         f2:{URL:'assets/ship2.png', name:'ship2'},
         f3:{URL:'assets/ship3.png', name:'ship3'}
     },
-    bullet:{URL:'assets/bullet.png', name:'bullet'},    
+    bullet:{URL:'assets/bullet.png', name:'bullet'},
     
     asteroidLarge:{URL:'assets/asteroidLarge.png', name:'asteroidLarge'},
     asteroidMedium:{URL:'assets/asteroidMedium.png', name:'asteroidMedium'},
@@ -27,6 +27,12 @@ const graphicAssets = {
         quick:{URL:'assets/powerupquickb.png', name:'quick',display:'Quick Bullets'},
         triple:{URL:'assets/poweruptripleb.png', name:'triple',display:'Triple Bullets'}
     },
+
+    enemies:{
+        station:{URL:'assets/enemy1.png', name:'station'},
+        shuttle:{URL:'assets/enemy1.png', name:'shuttle'},
+        turret:{URL:'assets/enemy1.png', name:'turret'}
+    }
 
     
 };
@@ -63,6 +69,23 @@ const asteroidProperties = {
 };
 
 
+const enemyProperties = {
+    
+    respawn:15,
+
+    bullet:{
+        speed: 500,
+        interval: 300,
+        lifeSpan: 1000,
+        maxCount: 30
+    },
+    
+    station: { minVelocity: 50, maxVelocity: 150, minAngularVelocity: 0, maxAngularVelocity: 200, score: 100, explosion:8 },
+    shuttle: { minVelocity: 100, maxVelocity: 200, minAngularVelocity: 0, maxAngularVelocity: 200, score: 150, explosion:4 },
+    turret: { minVelocity: 150, maxVelocity: 300, minAngularVelocity: 0, maxAngularVelocity: 0,score: 200, explosion:2 },
+
+};
+
 
 const powerupType = {
     none:0,
@@ -93,6 +116,8 @@ export default class GameScene extends Phaser.Scene{
         this.cursors = undefined;
         this.bulletGroup = undefined;
         this.asteroidGroup = undefined;
+        this.enemyGroup = undefined;
+        this.enemyBulletGroup = undefined;
         this.deathParticles = undefined;
         this.destroyParticles = undefined;
         this.powerup = powerupType.none;
@@ -102,6 +127,7 @@ export default class GameScene extends Phaser.Scene{
         this.ui_name = undefined;
         this.ui_power = undefined;
         this.bulletInterval = 0;
+        this.enemyBulletInterval = 0;
         this.asteroidsCount = asteroidProperties.startingAsteroids;
         this.shipLives = shipProperties.startingLives;
         this.score = 0;
@@ -115,6 +141,7 @@ export default class GameScene extends Phaser.Scene{
     }
     
     preload(){
+
         this.load.image(graphicAssets.asteroidLarge.name, graphicAssets.asteroidLarge.URL);
         this.load.image(graphicAssets.asteroidMedium.name, graphicAssets.asteroidMedium.URL);
         this.load.image(graphicAssets.asteroidSmall.name, graphicAssets.asteroidSmall.URL);
@@ -131,6 +158,10 @@ export default class GameScene extends Phaser.Scene{
         this.load.image(graphicAssets.powerups.quantum.name,graphicAssets.powerups.quantum.URL);
         this.load.image(graphicAssets.powerups.quick.name,graphicAssets.powerups.quick.URL);
         this.load.image(graphicAssets.powerups.triple.name,graphicAssets.powerups.triple.URL);
+
+        this.load.image(graphicAssets.enemies.station.name,graphicAssets.enemies.station.URL);
+        this.load.image(graphicAssets.enemies.shuttle.name,graphicAssets.enemies.shuttle.URL);
+        this.load.image(graphicAssets.enemies.turret.name,graphicAssets.enemies.turret.URL);
         
         
     };
@@ -140,6 +171,8 @@ export default class GameScene extends Phaser.Scene{
         this.createBulletGroup();
         this.createAsteroidGroup();
         this.createPowerupGroup();
+        this.createEnemyBulletGroup();
+        this.createEnemyGroup();
         this.createUI();
         this.cursors = this.input.keyboard.createCursorKeys()
         this.createParticles();
@@ -231,6 +264,35 @@ export default class GameScene extends Phaser.Scene{
 
     }
 
+    createEnemyBulletGroup(){
+        /** @type {Phaser.Physics.Arcade.Group} */
+        this.enemyBulletGroup = this.physics.add.group()
+        
+        this.enemyBulletGroup.createMultiple({active:false, quantity:bulletProperties.maxCount, key : graphicAssets.bullet.name});
+        
+        this.enemyBulletGroup.getChildren().forEach(function(sprite) {
+            sprite.disableBody(true,true);
+            sprite.state = objState.alive;
+        }, this);
+
+    }
+
+    createEnemyGroup(){
+        this.enemyGroup = this.physics.add.group();
+        this.enemyGroup.create(-5,-5,graphicAssets.enemies.station.name);
+        this.enemyGroup.create(-5,-5,graphicAssets.enemies.shuttle.name);
+        this.enemyGroup.create(-5,-5,graphicAssets.enemies.turret.name);
+
+        this.enemyGroup.getChildren().forEach(function(sprite) {
+            sprite.disableBody(true,true);
+            sprite.state = objState.alive;
+        }, this);
+
+        //this.physics.add.overlap(this.player,this.enemyGroup,enemyCollision,null,this);
+
+        this.time.delayedCall(2000,this.spawnEnemy,null,this);
+
+    }
 
 
     resetAsteroids() {
@@ -304,6 +366,7 @@ export default class GameScene extends Phaser.Scene{
 
         this.checkPlayerInput();
         this.wrapAround();
+        this.enemyFire();
         
 
         
@@ -456,6 +519,92 @@ export default class GameScene extends Phaser.Scene{
         //console.log(chosen);
 
         this.time.delayedCall(1000 * powerupProperties.respawn,this.respawnPowerup,null,this);
+    }
+
+    spawnEnemy(){
+        const rnd =Phaser.Math.RND;
+        var chosen = rnd.integerInRange(0,2);
+        var x,y;
+
+        if(rnd.integerInRange(0,1)){
+            x = 1 + rnd.integerInRange(0,1)*(this.game.canvas.width -2);
+            y = Math.random()*this.game.canvas.height;
+        }else{
+            y = 1 + rnd.integerInRange(0,1)*(this.game.canvas.height -2);
+            x = Math.random()*this.game.canvas.width;
+        }
+
+        if(chosen<this.enemyGroup.getLength()){
+            /** @type {Phaser.Physics.Arcade.Sprite} */
+            var enemy = this.enemyGroup.getFirstNth(chosen,false)
+            var type = enemy.texture.key;
+            
+            enemy.enableBody(true,x,y,true,true);
+            
+            enemy.setOrigin(0.5, 0.5);
+
+            enemy.body.angularVelocity = rnd.integerInRange(enemyProperties[type].minAngularVelocity, enemyProperties[type].maxAngularVelocity);
+            var randomVelocity = rnd.integerInRange(enemyProperties[type].minVelocity, enemyProperties[type].maxVelocity);
+            
+            var Angle = Phaser.Math.Angle.BetweenPoints(enemy.body.position,{x:this.game.canvas.width/2,y:this.game.canvas.height/2});
+
+            this.physics.velocityFromRotation(Angle, randomVelocity, enemy.body.velocity);
+
+        }
+
+        //console.log(chosen);
+
+        this.time.delayedCall(1000 * powerupProperties.respawn,this.respawnPowerup,null,this);
+
+    }
+
+    enemyFire(){
+
+        var enemy = this.enemyGroup.getFirst(true);
+
+        if(!enemy){
+            return;
+        }
+
+        if (this.time.now > this.enemyBulletInterval) {
+            
+            /** @type {Phaser.Physics.Arcade.Sprite} */
+            var bullet1 = this.enemyBulletGroup.getFirstNth(1,false)
+            var bullet2 = this.enemyBulletGroup.getFirstNth(2,false)
+            var bullet3 = this.enemyBulletGroup.getFirstNth(3,false)
+            if (bullet1) {
+                var length = enemy.width * 0.5;
+                var x = enemy.x + (Math.cos(enemy.rotation) * length);
+                var y = enemy.y + (Math.sin(enemy.rotation) * length);
+
+
+                bullet1.enableBody(true,x,y,true,true);
+                bullet1.rotation = enemy.rotation;
+                this.physics.velocityFromRotation(enemy.rotation, enemyProperties.bullet.speed, bullet1.body.velocity);
+                this.time.delayedCall(enemyProperties.bullet.lifeSpan,this.killMeNow,[bullet1],this);
+
+                if(this.powerup == powerupType.triple && bullet2 && bullet2){
+                    bullet2.enableBody(true,x,y,true,true);
+                    bullet3.enableBody(true,x,y,true,true);
+                    bullet2.rotation = this.player.rotation+15;
+                    bullet3.rotation = this.player.rotation-15;
+                    
+                    this.physics.velocityFromRotation(this.player.rotation+15, bulletProperties.speed, bullet2.body.velocity);
+                    this.physics.velocityFromRotation(this.player.rotation-15, bulletProperties.speed, bullet3.body.velocity);
+
+                    this.time.delayedCall(bulletProperties.lifeSpan,this.killMeNow,[bullet2],this);
+                    this.time.delayedCall(bulletProperties.lifeSpan,this.killMeNow,[bullet3],this);
+                }
+                
+                this.enemyBulletInterval = this.time.now + enemyProperties.bullet.interval;
+                if(this.powerup==powerupType.quantum){
+                    this.enemyBulletInterval+=500;
+                }else if(this.powerup==powerupType.quick){
+                    this.enemyBulletInterval-=200;
+                }
+            }
+        }
+
     }
 
     destroyShip() {
